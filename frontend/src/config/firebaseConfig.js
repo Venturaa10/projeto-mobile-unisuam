13860b8ec6237b5baec0e36c6aa68d258e13146e
Venturaa10 <joao.victor.ventura25@gmail.com>
2025-09-27 09:17:01 -0300

    - Configurar `db.js` para o meu notebook. - Atualizar dependencias. - Configurar o arquivo `firebaConfig.js` com as informações do Google Cloud. - Refatorar `api.ts` para o backend funcionar no mobile / Expo Go.

--- frontend/src/config/firebaseConfig.js
+++ frontend/src/config/firebaseConfig.js
@@ -1,17 +1,22 @@
-// src/config/firebaseConfig.js
+// config/firebaseConfig.ts
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCnW-tgu-zifneZ0596eyCNCqtewUfYXcU",
-  authDomain: "projeto-mobile-unisuam.firebaseapp.com",
-  projectId: "projeto-mobile-unisuam",
-  storageBucket: "projeto-mobile-unisuam.firebasestorage.app",
-  messagingSenderId: "502548654272",
-  appId: "1:502548654272:web:df3487b2c0bff193ca2525",
-  measurementId: "G-0FKWJ91M0L"
+  apiKey: "AIzaSyDqW-TJvVadsjCrK8QeLVz0NHJE0PZw8_g",
+  authDomain: "projeto-mobile-unisuam-473100.firebaseapp.com",
+  projectId: "projeto-mobile-unisuam-473100",
+  storageBucket: "projeto-mobile-unisuam-473100.appspot.com", // 👈 corrigi ".app" para ".appspot.com"
+  messagingSenderId: "659201367256",
+  appId: "1:659201367256:web:5e242dddeeeb1ac916cbad",
+  measurementId: "G-Y0EDEW3SNK", // pode deixar, mas não será usado no mobile
 };
 
-
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+export default app;
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
+import { Platform } from "react-native";
 
-// Define a tipagem do retorno do login
+// Tipagem do retorno do login
 export interface LoginResponse {
   token: string;
   tipo: "aluno" | "universidade";
@@ -11,16 +12,32 @@ export interface LoginResponse {
   };
 }
 
+// Configuração do IP local
+const LOCAL_IP = "192.168.1.74"; // IP da sua máquina na rede Wi-Fi
+
 // Cria instância do Axios
 // const api = axios.create({
-//   baseURL: "http://localhost:3000/api",
+//   baseURL:
+//     Platform.OS === "android"
+//       ? "http://10.0.2.2:3000/api"   
+//       : Platform.OS === "ios"
+//       ? `http://${LOCAL_IP}:3000/api` 
+//       : "http://localhost:3000/api",  
 //   headers: {
 //     "Content-Type": "application/json",
 //   },
 // });
 
+
+// axios
+//   .get("http://192.168.1.74:3000/api/alunos")
+//   .then(res => console.log("Conseguiu:", res.data))
+//   .catch(err => console.log("Erro mobile:", err));
+  
+
+// Configuração da api para funcionar o backend no mobile. 
 const api = axios.create({
-  baseURL: "http://1.0.11.21:3000/api",
+  baseURL: "http://192.168.1.74:3000/api",
   headers: {
     "Content-Type": "application/json",
   },
